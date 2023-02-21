1fe855780194094182d2a64ed026052ad291a49b
J T <jthompki@localhost.localdomain>
2023-02-20 22:39:01 -0500

    Added section for copying over oc client and installing

--- playbook.sh
+++ playbook.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ansible-playbook -i ./inventory microshift.yml --extra-vars "pull_secret_location=../pull-secret.txt uname=x86_64"
+ansible-playbook -i ./inventory microshift.yml --extra-vars "pull_secret_location=../pull-secret.txt uname=x86_64 oc_client_location=../oc_client.rpm oc_client=oc_client.rpm"
