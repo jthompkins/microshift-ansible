0ac3094ac77e5cce36471b46c275f5975e4db2c7
J T <jthompki@localhost.localdomain>
2023-02-21 10:20:11 -0500

    Added comment to script

--- playbook.sh
+++ playbook.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+# Add inventory file if missing
 ansible-playbook -i ./inventory microshift.yml --extra-vars "pull_secret_location=../pull-secret.txt uname=x86_64 oc_client_location=../oc_client.rpm oc_client=oc_client.rpm"
